e2d29816d4f5e9ea0baaba5d3e872d07399aed02
maxwellcavalli <maxwell.gobattocavalli@gmail.com>
2018-11-15 22:07:20 -0200

    Ajustes na posicao do botao salvar. Ajustes na criacao de botoes dentro de tabelas. Correcao de bug ao mostrar dialog. Adicionado opcao para cliente aparecer em todas as localidades (deploy). Correcao de bug na pesquisa de propagandas do cliente (deploy) - somente ativas). Adicionada regra para nao permitir excluir uma propaganda que faca parte de um deploy

---
+++ loto-core/src/main/java/br/com/loto/core/fx/datatable/interfaces/ICondidionalLabelButonColumn.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.loto.core.fx.datatable.interfaces;
+
+/**
+ *
+ * @author maxwe
+ */
+public interface IActionColumn<T> {
+    public void onAction(T t);
+}
